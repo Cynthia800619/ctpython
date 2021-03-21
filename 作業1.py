b2e0b7af73ce5c64ee40c66cdec0d3d593d997d2
Cynthia800619 <80256140+Cynthia800619@users.noreply.github.com>
2021-03-21 10:53:17 +0800

    Add files via upload

---
+++ 作業1.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 14 10:21:24 2021
+
+@author: user
+"""
+
+for i in range (5,0,-1):#[1,2,3,4]
+   
+    for a in range (0,i):#[1,2,3]
+        print("*",end="")
+    print()
+for i in range (2,6,1):#[1,2,3,4]
+   
+    for a in range (0,i):#[1,2,3]
+        print("*",end="")
+    print()
+print("程式執行完畢")    
